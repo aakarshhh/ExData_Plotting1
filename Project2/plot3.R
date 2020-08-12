a5945d227f06996788f0769cbec90b1d5c644f7c
Akarsh Shrivastava <akss4567@gmail.com>
2020-08-12 22:22:43 +0530

    Plot 3&4 added

---
+++ Project2/plot3.R
@@ -0,0 +1,17 @@
+library(data.table)
+library(tidyverse)
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
---
+++ Project2/plot4.R
@@ -0,0 +1,19 @@
+library("data.table")
+library(tidyverse)
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+combustionRelated <- grepl("comb", SCC[, SCC.Level.One], ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC[, SCC.Level.Four], ignore.case=TRUE) 
+combustionSCC <- SCC[combustionRelated & coalRelated, SCC]
+combustionNEI <- NEI[NEI[,SCC] %in% combustionSCC]
+
+png("plot4.png")
+
+ggplot(combustionNEI,aes(x = factor(year),y = Emissions/10^5)) +
+  geom_bar(stat="identity", fill ="#00FFFF", width=0.8) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+dev.off()
